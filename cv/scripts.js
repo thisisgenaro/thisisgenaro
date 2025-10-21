e6b04c3df04e446df61a3c6a335f58d6e894fbeb
thisisgenaro <71363975+thisisgenaro@users.noreply.github.com>
2025-10-21 00:49:50 -0400

    Fix class name in experience content
    Corrected class name from 'task-item' to 'talk-item'.

--- cv/scripts.js
+++ cv/scripts.js
@@ -38,7 +38,7 @@ function writeExperience(experience)
     var description = experience.description;
 
     var content = document.createElement('div');
-    content.setAttribute('class','task-item');
+    content.setAttribute('class','talk-item');
 
     var roleHTML = document.createElement('h3');
     roleHTML.innerText = role;
